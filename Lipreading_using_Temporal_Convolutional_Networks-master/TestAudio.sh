19eb22d147e4b000a5635cab4fc85304f6baee1a
kimud6003 <kimud6003@gmail.com>
2022-05-30 05:16:21 +0000

    shellScript

---
+++ Lipreading_using_Temporal_Convolutional_Networks-master/TestAudio.sh
@@ -0,0 +1,6 @@
+python main.py 
+--modality raw_audio \
+--config-path ./configs/lrw_resnet18_mstcn.json \
+--model-path ./models/lrw_resnet18_mstcn.pth.tar \
+--data-dir ./datasets/visual_data/ \
+--test
---
+++ Lipreading_using_Temporal_Convolutional_Networks-master/TestVisual.sh
@@ -0,0 +1,5 @@
+python main.py 
+--config-path ./configs/lrw_resnet18_mstcn.json \
+--model-path ./models/lrw_resnet18_mstcn.pth.tar \
+--data-dir ./datasets/visual_data/ \
+--test
