2a498dc2371e35880bde825022f4643dd5b1295e
HRPzz <royalpkgh@naver.com>
2022-06-01 23:12:48 +0000

    Update scripts

--- Lipreading_using_Temporal_Convolutional_Networks-master/CropMouth.sh
+++ Lipreading_using_Temporal_Convolutional_Networks-master/CropMouth.sh
@@ -1,7 +1,15 @@
+# python preprocessing/crop_mouth_from_video.py \
+# --video-direc ../sample/ \
+# --landmark-direc ./landmarks/LRW_landmarks/ \
+# --filename-path ../sample/AFTERNOON_detected_face.csv \
+# --save-direc ./datasets/visual_data/ \
+# --mean-face ./preprocessing/20words_mean_face.npy \
+# --convert-gray
+
 python preprocessing/crop_mouth_from_video.py \
---video-direc ../sample/ \
---landmark-direc ./landmarks/LRW_landmarks/ \
---filename-path ../sample/AFTERNOON_detected_face.csv \
+--video-direc ../hangul/ \
+--landmark-direc ./landmarks/hangul_landmarks/ \
+--filename-path ../hangul/오늘_detected_face.csv \
 --save-direc ./datasets/visual_data/ \
 --mean-face ./preprocessing/20words_mean_face.npy \
 --convert-gray
--- Lipreading_using_Temporal_Convolutional_Networks-master/ExtractAudio.sh
+++ Lipreading_using_Temporal_Convolutional_Networks-master/ExtractAudio.sh
@@ -1,4 +1,9 @@
+# python preprocessing/extract_audio_from_video.py \
+# --video-direc ../sample/ \
+# --filename-path ../sample/AFTERNOON_detected_face.csv \
+# --save-direc ./datasets/audio_data/ 
+
 python preprocessing/extract_audio_from_video.py \
---video-direc ../sample/ \
---filename-path ../sample/AFTERNOON_detected_face.csv \
+--video-direc ../hangul/ \
+--filename-path ../hangul/안녕하세요_detected_face.csv \
 --save-direc ./datasets/audio_data/ 
