c15b5ec7f76c1b406bff1741c185217394568252
HRPzz <royalpkgh@naver.com>
2022-06-01 23:18:25 +0000

    Update scripts

--- Lipreading_using_Temporal_Convolutional_Networks-master/TrainAudio.sh
+++ Lipreading_using_Temporal_Convolutional_Networks-master/TrainAudio.sh
@@ -1,5 +1,11 @@
+# python main.py \
+# --modality raw_audio \
+# --config-path ./configs/lrw_resnet18_mstcn.json \
+# # --annonation-direc ../sample/ \
+# --data-dir ./datasets/audio_data
+
 python main.py \
 --modality raw_audio \
 --config-path ./configs/lrw_resnet18_mstcn.json \
---annonation-direc ../sample/ \
+--annonation-direc ../hangul/ \
 --data-dir ./datasets/audio_data
